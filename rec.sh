bf692a8ece37b8226789503d540b24c2e9ff1bcc
Samuele Musiani <samu@teapot.ovh>
2024-05-29 22:22:25 +0200

    pandoc can't work in parallel

--- rec.sh
+++ rec.sh
@@ -7,4 +7,4 @@
 # This programs takes 3 arguments: the convert script, the regex and the 
 # program used for the conversion.
 
-find . -print0 | xargs -r -0 -P "$(nproc)" -I{} "$1" '{}' "$2" "$3"
+find . -print0 | xargs -r -0 -I{} "$1" '{}' "$2" "$3"
