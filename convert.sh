f9ecc21b1abd90dc82b2adbc002356e5b665c6d3
VaiTon <eyadlorenzo@gmail.com>
2025-06-22 12:46:42 +0200

    Add SPDX license headers and improve error handling in convert.sh

--- convert.sh
+++ convert.sh
@@ -1,16 +1,32 @@
 #!/bin/sh
-set -x
 
-# This script is called by ./rec and is used to actually check and convert a 
-# file. It takes 3 arguments: the file path, the regex that needs to match the 
+# SPDX-FileCopyrightText: 2024 Samuele Musiani <samu@teapot.ovh>
+# SPDX-FileCopyrightText: 2025 Eyad Issa <eyadlorenzo@gmail.com>
+#
+# SPDX-License-Identifier: AGPL-3.0-only
+
+# This script is called by ./rec and is used to actually check and convert a
+# file. It takes 3 arguments: the file path, the regex that needs to match the
 # file and the program used for the conversion.
 
-echo "$1" | grep -E "$2" > /dev/null 
+set -e # Exit on error
+set -u # Treat unset variables as an error
+
+if [ "$#" -ne 3 ]; then
+  echo "Usage: $0 <file_path> <regex> <conversion_program>" >&2
+  exit 1
+fi
+
+echo "$1" | grep -E "$2" >/dev/null
 MATCH=$?
 
 if [ $MATCH -eq 0 ]; then
   cd "$(dirname "$1")" || exit 1
+
+  set -x # Print commands
   $3 "$(basename "$1")"
+  set +x # Stop printing commands
+
 elif [ $MATCH -ne 1 ]; then
   exit 1
 fi
