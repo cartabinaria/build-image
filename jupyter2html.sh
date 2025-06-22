13529a55d099d839985a9ff775076e63b8f6ca9d
VaiTon <eyadlorenzo@gmail.com>
2025-06-22 12:46:45 +0200

    Add copyright and license headers to jupyter2html.sh

--- jupyter2html.sh
+++ jupyter2html.sh
@@ -1,3 +1,17 @@
-#!/bin/sh -ex
+#!/bin/sh
 
+# SPDX-FileCopyrightText: 2024 Samuele Musiani <samu@teapot.ovh>
+#
+# SPDX-License-Identifier: AGPL-3.0-only
+
+set -e # Exit on error
+set -u # Treat unset variables as an error
+
+if [ "$#" -ne 1 ]; then
+  echo "This script converts a Jupyter notebook to HTML." >&2
+  echo "Usage: $0 <notebook.ipynb>" >&2
+  exit 1
+fi
+
+set -x # Print commands
 /opt/venv/bin/jupyter nbconvert --to html "$1" --HTMLExporter.theme=dark
