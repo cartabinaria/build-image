88ee95295e1cd91e5d1eced2121854fbf89701ff
VaiTon <eyadlorenzo@gmail.com>
2025-06-22 12:30:41 +0200

    Add usage info, copyright, and improve argument handling in rec

--- rec.sh
+++ rec.sh
@@ -1,10 +1,19 @@
-#!/bin/sh -ex
+#!/bin/sh
 
-# The chaining of find and grep was deemded necessary due to find's lack of a
-# proper regex implementation. Also note that on linux find does not support
-# -E like on BSDs.
+# SPDX-FileCopyrightText: 2022 Luca Tagliavini <luca.tagliavini5@studio.unibo.it>
+#
+# SPDX-License-Identifier: AGPL-3.0-only
 
-# This programs takes 3 arguments: the convert script, the regex and the 
+# This programs takes 3 arguments: the convert script, the regex and the
 # program used for the conversion.
+
+set -e # Exit on error
+set -u # Treat unset variables as an error
+
+# check the number of arguments
+if [ "$#" -ne 3 ]; then
+  echo "Usage: $0 <convert_script> <regex> <program>"
+  exit 1
+fi
 
 find . -print0 | xargs -r -0 -I{} "$1" '{}' "$2" "$3"
